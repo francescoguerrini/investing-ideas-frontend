4e54c99b3db3b3ec867f488052ee0c42cab0181f
francescoguerrini <francescoguerrini1987@gmail.com>
2025-01-13 15:48:19 +0100

    render url

--- app/utils/fetcher.js
+++ app/utils/fetcher.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const URL = "https://investing-ideas-backend.onrender.com/";
+
 // Funzione fetcher per ottenere i dati dell'azienda
 export const fetchCompanyData = async (ticker, updateCompanyData) => {
   try {
-    const response = await axios.get(`http://localhost:8000/company/${ticker}`);
+    const response = await axios.get(`${URL}/company/${ticker}`);
     console.log("TICKER:", ticker);
     if (response.data) {
       updateCompanyData(response.data);
